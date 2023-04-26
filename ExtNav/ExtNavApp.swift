d2649cd20343c3408b11affb0dcf5dde416f17a6
DaianaM <daiana.m.0708@gmail.com>
2023-04-26 16:49:18 -0400

    Initial Commit

---
+++ ExtNav/ExtNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExtNavApp.swift
+//  ExtNav
+//
+//  Created by scholar on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ExtNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
